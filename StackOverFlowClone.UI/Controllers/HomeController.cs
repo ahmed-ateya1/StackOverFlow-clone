51a79bc535887aa2bc85da0c2d3301a5d91f49a8
Ahmed Ateya <138663251+ahmed-ateya1@users.noreply.github.com>
2024-08-11 04:18:10 +0300

    Creat-CleanArc-Layers

---
+++ StackOverFlowClone.UI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using StackOverFlowClone.UI.Models;
+using System.Diagnostics;
+
+namespace StackOverFlowClone.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StackOverFlowClone.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StackOverFlowClone.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
